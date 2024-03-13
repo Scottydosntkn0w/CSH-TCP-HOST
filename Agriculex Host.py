bdc48a147ef7052edb35bf4cf657b358b7b6c9aa
Scottydosntkn0w <scott.hendricks1568@gmail.com>
2024-03-13 14:18:59 -0500

    implement db specific write

--- Agriculex Host.py
+++ Agriculex Host.py
@@ -1,34 +1,91 @@
-# multiconn-server.py
+#!/usr/bin/env python3
 
 import sys
 import socket
 import selectors
 import types
+import time
+import mysql.connector
 
 sel = selectors.DefaultSelector()
-host = ""
-port = 50000
-#host, port = sys.argv[1], int(sys.argv[2])
-lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lsock.bind((host, port))
-lsock.listen()
-print(f"Listening on {(host, port)}")
-lsock.setblocking(False)
-sel.register(lsock, selectors.EVENT_READ, data=None)
 
-def main():
+
+host="10.1.175.40"
+user="iotadmin"
+password="controlsadmin"
+database="CSH Production"
+
+
+#Example
+""" 
+Echoing b'pack:   count: 400\n' to ('10.35.1.62', 45170)
+Echoing b'pack: PED00053460_3  count: 250\n' to ('10.35.1.60', 52464)
+Echoing b'pack:  ount: 400\n' to ('10.35.1.62', 45170)
+Echoing b'pack: PED00053460_4  count: 250\n' to ('10.35.1.60', 52464)
+Echoing b'pack: PED00053460_5  count: 250\n' to ('10.35.1.60', 52464)
+Echoing b'pack: PED00053460_6  count: 250\n' to ('10.35.1.60', 52464)
+Echoing b'pack:   count: 400\n' to ('10.35.1.62', 45170)
+Echoing b'pack: PED00053460_7  count: 250\n' to ('10.35.1.60', 52464) 
+
+Echoing b'dump: 0\n\n' to ('10.35.1.60', 40790)
+Echoing b'dump: 0\n\n' to ('10.35.1.60', 40790)
+Echoing b'pack: 7659367_10279  count: 250\n' to ('10.35.1.60', 40790)
+Echoing b'pack: I466574  count: 250\n' to ('10.35.1.60', 40790)
+Echoing b'pack: 7669085_11677  count: 250\n' to ('10.35.1.60', 40790)
+
+"""
+
+#{data.outb!r} = pack: PED00053460_7  count: 250\n
+#{data.addr} = ('10.35.1.60', 52464)
+
+def parseData(data):
+    outputString = data.outb 
+    machine_ip = data.addr[0]
+
+    outputString = outputString.strip("\n")
+    outputString = outputString.strip(":")
+    outputList = outputString.split(" ")
+
+    action = outputList[0]
+
+    if action == "dump":
+        plant_name = ""
+        seed_count = outputList[1]
+
+    if action == "pack":
+        if len(outputList) == 4:
+            plant_name = outputList[1]
+            seed_count = outputList[3]
+        if len(outputList) == 3:
+            plant_name = ""
+            seed_count = outputList[2]
+
+    datalist = (action, plant_name, seed_count, machine_ip)
+
+    return datalist
+
+def writeToDb(data):
     try:
-        while True:
-            events = sel.select(timeout=None)
-            for key, mask in events:
-                if key.data is None:
-                    accept_wrapper(key.fileobj)
-                else:
-                    service_connection(key, mask)
-    except KeyboardInterrupt:
-        print("Caught keyboard interrupt, exiting")
-    finally:
-        sel.close()
+        datalist = parseData(data)
+    except:
+        print("parse data failed")
+
+    mydb = mysql.connector.connect(
+        host,
+        user,
+        password,
+        database
+    )
+
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO Agriculex_Raw (`Action`, `Time`, `Plant Name`, Count, `I.P.`) VALUES (%s, %s, %s, %i, %s)" 
+    val = (datalist[0], time.time(),  datalist[1], datalist[2], datalist[3])
+
+    mycursor.execute(sql, val)
+
+    mydb.commit
+    mydb.close
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()  # Should be ready to read
@@ -38,13 +95,6 @@ def accept_wrapper(sock):
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
 
 def service_connection(key, mask):
     sock = key.fileobj
@@ -53,6 +103,12 @@ def service_connection(key, mask):
         recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
             data.outb += recv_data
+            try:
+                writeToDb(data)
+            except:
+                print("error in write to Db")
+
+
         else:
             print(f"Closing connection to {data.addr}")
             sel.unregister(sock)
@@ -63,5 +119,28 @@ def service_connection(key, mask):
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
-if __name__ == "__main__":
-    main()
+
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <host> <port>")
+    sys.exit(1)
+
+host, port = sys.argv[1], int(sys.argv[2])
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
